b01bc9a86d464ab21c1916b8cb7f67c55a101fe5
Nicky Rakotoarimanana <72654839+Ykcin360@users.noreply.github.com>
2023-06-05 12:01:12 +0300

    Update modals.blade.php
    Deactivate the update avatar button on Chatify

--- resources/views/vendor/Chatify/layouts/modals.blade.php
+++ resources/views/vendor/Chatify/layouts/modals.blade.php
@@ -42,10 +42,10 @@
                       style="background-image: url('{{ Chatify::getUserWithAvatar(Auth::user())->avatar }}');"
                       ></div>
                       <p class="upload-avatar-details"></p>
-                      <label class="app-btn a-btn-primary update" style="background-color:{{$messengerColor}}">
+                      {{-- <label class="app-btn a-btn-primary update" style="background-color:{{$messengerColor}}">
                           Upload New
                           <input class="upload-avatar chatify-d-none" accept="image/*" name="avatar" type="file" />
-                      </label>
+                      </label> --}}
                       {{-- Dark/Light Mode  --}}
                       <p class="divider"></p>
                       <p class="app-modal-header">Dark Mode <span class="
